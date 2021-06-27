84068faeac1833b44001321ffb81abbf7b4a449e
lassaadbaati <lassaad.baati@gmail.com>
2021-06-27 11:03:11 +0100

    time

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Formation JENKINS uit!");
+        System.out.println("Hello, Formation JENKINS uit! 11h00 ");
     }
 }
